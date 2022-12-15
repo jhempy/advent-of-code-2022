4bb716bb40cf3c23aee9454a3ddd695dc288382e
Janine Hempy <jhempy@gmail.com>
2022-12-14 23:58:19 -0500

    Day10 complete

--- Day10/part2.php
+++ Day10/part2.php
@@ -10,11 +10,14 @@ fclose($input);
 
 $instructions = explode("\n", $raw);
 
-$x = 1;
+$x = 1; // sprite position (center of 3 pixels)
 $c = 0;
 
 $strength_indices = [20, 60, 100, 140, 180, 220];
 $signal_strength = array();
+
+$screen = array_fill( 0, 240, ' ' );
+// print_screen();
 
 foreach ($instructions as $code)
 {
@@ -33,10 +36,10 @@ foreach ($instructions as $code)
 	}
 }
 
-print_r($signal_strengths);
+// print_r($signal_strengths);
+// echo "Sum of signal strengths is ", array_sum($signal_strengths), "\n";
 
-
-echo "Sum of signal strengths is ", array_sum($signal_strengths), "\n";
+print_screen();
 
 echo "\n\nDone!\n";
 
@@ -58,11 +61,38 @@ function tick($delta)
 		$signal_strengths[$c] = $temp;
 	}
 
+	// draw pixel during cycle
+	draw_pixel();
+
 	$x += $delta;
 	echo "\tAt the end of cycle $c, the register is $x\n";
 }
 
+function draw_pixel()
+{
+	global $c, $x, $screen;
+	$p = ($c - 1) % 40; // $pixels start at 0, cycles start at 1
+	echo "\tDrawing in cycle/position $c with register value $x\n";
+	if ( ( $p == $x - 1 ) || ( $p == $x ) || ( $p == $x + 1 ) ) {
+		// pixel is lit
+		$screen[$c-1] = '#';
+	}
+}
 
+
+function print_screen() 
+{
+	global $screen;
+	echo "\n";
+	for ( $i = 0; $i < 240; $i++ )
+	{
+		echo $screen[$i];
+		if ( $i % 40 == 39 ){
+			echo "\n";
+		}
+	}
+	echo "\n";
+}
 
 
 ?>
